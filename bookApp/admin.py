47304a7506404e9ce829f05dce8e08397d4266ab
ElijahSze <elijah_sze@163.com>
2020-07-22 21:11:27 +0800

    Django第二次更改：添加了数据库

--- bookApp/admin.py
+++ bookApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Book, Hero
 # Register your models here.
+# 后台管理的配置文件
+# Register your models here.
+admin.site.register(Book)
+admin.site.register(Hero)
